fe8d3fd95a104f64d5f6a99b31f44ca6b353fc7b
2kids <pear9123@gmail.com>
2021-07-19 17:05:49 +0900

    add controller

---
+++ src/main/java/com/ex/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.ex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/test")
+	public String test(Model model) {
+		
+		model.addAttribute("msg", "TEST12345");
+		
+		return "test";
+	}
+}
