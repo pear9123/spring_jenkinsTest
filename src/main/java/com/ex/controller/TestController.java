e9cc1a5dea04e4ad9d95e8fc1bcee1a3c98df1a2
2kids <pear9123@gmail.com>
2021-07-20 10:30:28 +0900

    add method test2

--- src/main/java/com/ex/controller/TestController.java
+++ src/main/java/com/ex/controller/TestController.java
@@ -9,9 +9,13 @@ public class TestController {
 
 	@RequestMapping("/test")
 	public String test(Model model) {
-		
 		model.addAttribute("msg", "TEST12345");
-		
 		return "test";
+	}
+	
+	@RequestMapping("/test2")
+	public String test2(Model model) {
+		model.addAttribute("msg", "D2Model");
+		return "test2";
 	}
 }
