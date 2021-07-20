6d7247b3d9063f0ed6e3a9275662717d3e75de22
pear9123 <pear9123@naver.com>
2021-07-20 13:51:22 +0900

    main add method main

---
+++ src/main/java/com/ex/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.ex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/main")
+	public String main(Model model) {
+		return "home";
+	}
+}
